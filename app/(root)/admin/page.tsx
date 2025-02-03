2be4050ec34fd886b7290b2628a35a45264781bc
CodeEternalCode <codeeternal@gmail.com>
2025-02-03 23:31:50 +0500

    Added admin panel and quran participants details page

--- app/(root)/admin/page.tsx
+++ app/(root)/admin/page.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import ServiceTable from "@/components/admin/ServiceTable";
+import QuranCompetitionTable from "@/components/quran-competition/QuranCompetitionTable";
 
 const Admin = () => {
   return (
     <section className="h-screen p-8 container mx-auto">
       <div className="mt-8">
-        <div>
+        {/* <div>
           <h2 className="text-3xl font-dhivehi mb-10 text-right text-cyan-950">
             ރެޖިސްޓްރޭޝަންސް
           </h2>
           <ServiceTable />
-        </div>
-
-        {/* <div className="mt-10">
-          <h2 className="text-3xl font-dhivehi mb-10 text-right text-cyan-950">
-            ސާވިސް ރިކުއެސްޓްސް
-          </h2>
         </div> */}
+
+        <div className="mt-10">
+          <h2 className="text-3xl font-dhivehi mb-10 text-right text-cyan-950">
+            ޤުރުއާން މުބާރާތުގެ ބައިވެރިން
+          </h2>
+
+          <QuranCompetitionTable />
+        </div>
       </div>
     </section>
   );
