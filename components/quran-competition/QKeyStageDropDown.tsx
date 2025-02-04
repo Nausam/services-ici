c59889e15fd00a9234a0c5d4cf15bf42732618a0
CodeEternalCode <codeeternal@gmail.com>
2025-02-04 10:11:47 +0500

    Added keystage select

---
+++ components/quran-competition/QKeyStageDropDown.tsx
@@ -0,0 +1,79 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { startTransition, useEffect, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+type DropdownProps = {
+  value?: string;
+  onChangeHandler?: (value: string) => void;
+};
+
+const QKeyStageDropDown = ({ value, onChangeHandler }: DropdownProps) => {
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field flex justify-end font-dhivehi ">
+        <SelectValue className="text-slate-100" placeholder=" ކީސްޓޭޖް " />
+      </SelectTrigger>
+      <SelectContent
+        dir="rtl"
+        className="font-dhivehi  text-gray-500 text-right"
+      >
+        <AlertDialog>
+          <SelectItem
+            className="cursor-pointer"
+            value="ޚާއްޞައެހީއަށް ބޭނުންވާ ފަރާތްތައް"
+          >
+            ޚާއްޞައެހީއަށް ބޭނުންވާ ފަރާތްތައް
+          </SelectItem>
+          <SelectItem className="cursor-pointer" value="ބޭބީ ނާސަރީ، ނާސަރީ">
+            ބޭބީ ނާސަރީ، ނާސަރީ
+          </SelectItem>
+          <SelectItem className="cursor-pointer" value="ފައުންޑޭޝަން">
+            ފައުންޑޭޝަން
+          </SelectItem>
+          <SelectItem className="cursor-pointer" value="ކީ ސްޓޭޖް 1">
+            ކީ ސްޓޭޖް 1
+          </SelectItem>
+          <SelectItem className="cursor-pointer" value="ކީ ސްޓޭޖް 2">
+            ކީ ސްޓޭޖް 2
+          </SelectItem>
+          <SelectItem className="cursor-pointer" value="ކީ ސްޓޭޖް 3">
+            ކީ ސްޓޭޖް 3
+          </SelectItem>
+          <SelectItem className="cursor-pointer" value="ކީ ސްޓޭޖް 4">
+            ކީ ސްޓޭޖް 4
+          </SelectItem>
+          <SelectItem className="cursor-pointer" value="ކީ ސްޓޭޖް 5">
+            ކީ ސްޓޭޖް 5
+          </SelectItem>
+          <SelectItem
+            className="cursor-pointer"
+            value="18އަހަރުން މަތި (އާއްމުންގެ ބައި) "
+          >
+            18 އަހަރުން މަތި (އާންމުންގެ ބައި)
+          </SelectItem>
+          <SelectItem className="cursor-pointer" value="ޙާފިޡުންގެ ބައި">
+            ޙާފިޡުންގެ ބައި
+          </SelectItem>
+        </AlertDialog>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default QKeyStageDropDown;
