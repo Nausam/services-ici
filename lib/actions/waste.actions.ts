fe5c598464eb5dedd55a42b2ff1940fe2d87e084
CodeEternalCode <codeeternal@gmail.com>
2025-02-12 15:31:07 +0500

    Fix 1

--- lib/actions/waste.actions.ts
+++ lib/actions/waste.actions.ts
@@ -24,7 +24,7 @@ export const uploadImage = async (file: File): Promise<string> => {
     const permissions = [Permission.read(Role.any())];
 
     const bucketFile = await storage.createFile(
-      appwriteConfig.wasteManagementBucket,
+      appwriteConfig.bucketId,
       ID.unique(),
       inputFile,
       permissions
