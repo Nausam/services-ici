17d5e485380a826eb53d1a67d476aa5b91fba46c
CodeEternalCode <codeeternal@gmail.com>
2025-02-19 19:25:06 +0500

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,51 @@ export const metadata: Metadata = {
   title: "ICI Services Portal",
   description:
     "Explore the Innamaadhoo Council Services Portal – your gateway to seamless service requests, registrations, and updates for community development and sustainability. Join us in building a cleaner, more connected, and vibrant Innamaadhoo.",
+  keywords: [
+    "Innamaadhoo",
+    "Council Services",
+    "Maldives",
+    "Community Development",
+    "Waste Management",
+    "Registrations",
+    "Local Government",
+    "ICI Services",
+    "Sustainability",
+    "Public Services",
+  ],
+  authors: [{ name: "Innamaadhoo Council" }],
+  openGraph: {
+    title: "ICI Services Portal",
+    description:
+      "Your gateway to seamless service requests, registrations, and community updates in Innamaadhoo.",
+    url: "https://services.ici.gov.mv",
+    type: "website",
+    images: [
+      {
+        url: "/assets/images/seo_image.png",
+        width: 1200,
+        height: 630,
+        alt: "Waste Management Banner",
+      },
+    ],
+    locale: "dv_MV",
+    siteName: "ICI Services Portal",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ICI Services Portal",
+    description:
+      "Your gateway to seamless service requests, registrations, and community updates in Innamaadhoo.",
+    images: "/assets/images/seo_image.png",
+  },
+  metadataBase: new URL("https://services.ici.gov.mv"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      en: "/en",
+      dv: "/dv",
+    },
+  },
 };
 
 export default function RootLayout({
